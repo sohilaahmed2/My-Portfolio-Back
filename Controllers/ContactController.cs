afb6ee218b3b8d671068088ccd9c81821faa1257
Sohila <sohila24ahmed@gmail.com>
2026-04-13 22:31:00 +0200

    smtp4

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using portfolio.Models;
-using System.Net.Mail;
 using System.Net;
-using Microsoft.AspNetCore.Cors;
+using System.Net.Mail;
 
 namespace portfolio.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [EnableCors("AllowAll")]
     public class ContactController : ControllerBase
     {
         [HttpPost("send")]
@@ -17,22 +14,27 @@ namespace portfolio.Controllers
         {
             try
             {
-                var smtpClient = new SmtpClient("smtp.gmail.com")
+                var smtpUser = Environment.GetEnvironmentVariable("SMTP_USER");
+                var smtpPass = Environment.GetEnvironmentVariable("SMTP_PASS");
+
+                var smtpClient = new SmtpClient("smtp-relay.brevo.com")
                 {
                     Port = 587,
-                    Credentials = new NetworkCredential("sohila24ahmed@gmail.com", "lezf tstz ndid eriz"),
+                    Credentials = new NetworkCredential(smtpUser, smtpPass),
                     EnableSsl = true,
+                    UseDefaultCredentials = false
                 };
 
                 var mailMessage = new MailMessage
                 {
-                    From = new MailAddress("sohila24ahmed@gmail.com"),
+                    From = new MailAddress(smtpUser),
                     Subject = $"New message from {form.Name}",
                     Body = $"From: {form.Name}\nEmail: {form.Email}\n\n{form.Message}",
                     IsBodyHtml = false,
                 };
+
                 mailMessage.ReplyToList.Add(new MailAddress(form.Email));
-                mailMessage.To.Add("sohila24ahmed@gmail.com");
+                mailMessage.To.Add(smtpUser);
 
                 smtpClient.Send(mailMessage);
 
@@ -44,5 +46,4 @@ namespace portfolio.Controllers
             }
         }
     }
-
 }
